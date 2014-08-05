49f6b32cb2f72a61e0993b86f73d4f8f2f1a4aee
nadavsinai <ns@nadavsinai.com>
2014-08-05 12:17:29 +0300

    update to element and some more internal changes

--- src/ng-bs-daterangepicker.js
+++ src/ng-bs-daterangepicker.js
@@ -1,70 +1,69 @@
 /**
  * @license ng-bs-daterangepicker v0.0.1
- * (c) 2013 Luis Farzati http://github.com/luisfarzati/ng-bs-daterangepicker
+ * @forked by StartAppDev (Nadav Sinai,misterBit)
+ @original Author* (c) 2013 Luis Farzati http://github.com/luisfarzati/ng-bs-daterangepicker
  * License: MIT
  */
 (function (angular) {
-'use strict';
+    'use strict';
 
-angular.module('ngBootstrap', []).directive('input', function ($compile, $parse) {
-	return {
-		restrict: 'E',
-		require: '?ngModel',
-		link: function ($scope, $element, $attributes, ngModel) {
-			if ($attributes.type !== 'daterange' || ngModel === null ) return;
+    angular.module('ngDateRange', []).directive('ngDateRange', function ($compile, $parse) {
+        return {
+            restrict: 'EA',
+            require: '?ngModel',
+            link: function ($scope, $element, $attributes, ngModel) {
+                if (ngModel === null) return;
 
-			var options = {};
-			options.format = $attributes.format || 'YYYY-MM-DD';
-			options.separator = $attributes.separator || ' - ';
-			options.minDate = $attributes.minDate && moment($attributes.minDate);
-			options.maxDate = $attributes.maxDate && moment($attributes.maxDate);
-			options.dateLimit = $attributes.limit && moment.duration.apply(this, $attributes.limit.split(' ').map(function (elem, index) { return index === 0 && parseInt(elem, 10) || elem; }) );
-			options.ranges = $attributes.ranges && $parse($attributes.ranges)($scope);
-			options.locale = $attributes.locale && $parse($attributes.locale)($scope);
-			options.opens = $attributes.opens && $parse($attributes.opens)($scope);
+                var options = {};
+                options.format = $attributes.format || 'YYYY-MM-DD';
+                options.separator = $attributes.separator || ' - ';
+                options.minDate = $attributes.minDate && moment($attributes.minDate);
+                options.maxDate = $attributes.maxDate && moment($attributes.maxDate);
+                options.dateLimit = $attributes.limit && moment.duration.apply(this, $attributes.limit.split(' ').map(function (elem, index) {
+                    return index === 0 && parseInt(elem, 10) || elem;
+                }));
+                options.ranges = $attributes.ranges && $parse($attributes.ranges)($scope);
+                options.locale = $attributes.locale && $parse($attributes.locale)($scope);
+                options.opens = $attributes.opens && $parse($attributes.opens)($scope);
 
-			function format(date) {
-				return date.format(options.format);
-			}
+                function format(date) {
+                    return date.format(options.format);
+                }
 
-			function formatted(dates) {
-				return [format(dates.startDate), format(dates.endDate)].join(options.separator);
-			}
+                function formatted(dates) {
+                    return [format(dates.startDate), format(dates.endDate)].join(options.separator);
+                }
 
-			ngModel.$formatters.unshift(function (modelValue) {
-				if (!modelValue) return '';
-				return modelValue;
-			});
+                var updateCalender = function (modelValue) {
+                    if (!modelValue || (!modelValue.startDate)) {
+                        ngModel.$setViewValue({startDate: moment().startOf('day'), endDate: moment().startOf('day')});
+                        return;
+                    }
+                    $element.data('daterangepicker').startDate = modelValue.startDate;
+                    $element.data('daterangepicker').endDate = modelValue.endDate;
+                    $element.data('daterangepicker').updateView();
+                    $element.data('daterangepicker').updateCalendars();
+                    $element.data('daterangepicker').updateInputText();
+                    return modelValue;
+                };
 
-			ngModel.$parsers.unshift(function (viewValue) {
-				return viewValue;
-			});
+                ngModel.$formatters.unshift(updateCalender);
 
-			ngModel.$render = function () {
-				if (!ngModel.$viewValue || !ngModel.$viewValue.startDate) return;
-				$element.val(formatted(ngModel.$viewValue));
-			};
+                ngModel.$parsers.unshift(updateCalender)
 
-			$scope.$watch($attributes.ngModel, function (modelValue) {
-				if (!modelValue || (!modelValue.startDate)) {
-					ngModel.$setViewValue({ startDate: moment().startOf('day'), endDate: moment().startOf('day') });
-					return;
-				}
-				$element.data('daterangepicker').startDate = modelValue.startDate;
-				$element.data('daterangepicker').endDate = modelValue.endDate;
-				$element.data('daterangepicker').updateView();
-				$element.data('daterangepicker').updateCalendars();
-				$element.data('daterangepicker').updateInputText();
-			});
+                ngModel.$render = function () {
+                    if (!ngModel.$viewValue || !ngModel.$viewValue.startDate) return;
+                    $element.val(formatted(ngModel.$viewValue));
+                };
 
-			$element.daterangepicker(options, function(start, end) {
-				$scope.$apply(function () {
-					ngModel.$setViewValue({ startDate: start, endDate: end });
-					ngModel.$render();
-				});
-			});			
-		}
-	};
-});
+                $element.daterangepicker(options, function (start, end) {
+                    $scope.$apply(function () {
+                        ngModel.$setViewValue({startDate: start, endDate: end});
+                        ngModel.$render();
+                    });
+                });
+            }
+        };
+    });
 
 })(angular);
